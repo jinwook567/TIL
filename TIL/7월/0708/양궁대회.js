d61ba02a258fe54e54f3c8bf89af72a7dba720e7
jinwook567 <jinwook567@we2d.com>
2022-07-09 02:38:39 +0900

    0708 TIL

---
+++ TIL/7월/0708/양궁대회.js
@@ -0,0 +1,62 @@
+//같은 숫자에 넣을거면 1개를 더 넣어줘야한다.
+//아니라면 1개만 넣어주면 된다.
+
+console.log(11 ** 8);
+function getScore(info, lion) {
+  let appeachScore = 0;
+  let lionScore = 0;
+
+  for (let i = 0; i <= 10; i++) {
+    if (info[i] >= lion[i]) {
+      appeachScore += 10 - i;
+    } else {
+      lionScore += 10 - i;
+    }
+  }
+
+  return { appeachScore, lionScore };
+}
+
+function getPermutation(arr, n) {
+  const result = [];
+  if (n === 1) return arr.map((el) => [el]);
+
+  arr.forEach((v) => {
+    const rest = [...arr];
+    const permutation = getPermutation(rest, n - 1);
+    const attached = permutation.map((el) => [v, ...el]);
+    result.push(...attached);
+  });
+  return result;
+}
+
+function solution(n, info) {
+  let max = 0;
+  let answer;
+
+  const numbers = [10, 9, 8, 7, 6, 5, 4, 3];
+
+  const permutation = getPermutation(numbers, n);
+  permutation.forEach((arr) => {
+    console.log(arr);
+    const lion = Array(11).fill(0);
+    arr.forEach((v) => {
+      //i는 10-i라고 했습니다.
+      lion[10 - v] += 1;
+    });
+
+    //d는 라이언이 쏜 경우의 수들
+    const { appeachScore, lionScore } = getScore(info, lion);
+    if (lionScore > appeachScore) {
+      if (lionScore > max) {
+        max = lionScore;
+        answer = lion;
+      }
+    }
+  });
+
+  return answer ? answer : [-1];
+}
+
+const r = solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]);
+console.log({ r });
